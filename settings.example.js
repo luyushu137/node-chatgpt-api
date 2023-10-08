4e42158d327fa2e1b4640a2439960c4da3689ee6
luyushu137 <147305101+luyushu137@users.noreply.github.com>
2023-10-08 17:15:07 +0800

    Update settings.examplev1.js

--- settings.example.js
+++ settings.example.js
@@ -2,13 +2,13 @@ export default {
     // Options for the Keyv cache, see https://www.npmjs.com/package/keyv.
     // This is used for storing conversations, and supports additional drivers (conversations are stored in memory by default).
     // Only necessary when using `ChatGPTClient`, or `BingAIClient` in jailbreak mode.
-    cacheOptions: {},
+    cacheOptions: {ChatGPTClient},
     // If set, `ChatGPTClient` and `BingAIClient` will use `keyv-file` to store conversations to this JSON file instead of in memory.
     // However, `cacheOptions.store` will override this if set
     storageFilePath: process.env.STORAGE_FILE_PATH || './cache.json',
     chatGptClient: {
         // Your OpenAI API key (for `ChatGPTClient`)
-        openaiApiKey: process.env.OPENAI_API_KEY || '',
+        openaiApiKey: process.env.OPENAI_API_KEY || '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',
         // (Optional) Support for a reverse proxy for the completions endpoint (private API server).
         // Warning: This will expose your `openaiApiKey` to a third party. Consider the risks before using this.
         // reverseProxyUrl: 'https://chatgpt.hato.ai/completions',
